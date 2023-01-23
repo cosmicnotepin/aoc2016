34003bbea9f29e2438e50173c06c9a57eae68736
dmad <dmad@t14g3.fritz.box>
2023-01-23 21:20:29 +0100

    after i learned about valarray[slice] and sscanf

--- 8/main.cpp
+++ 8/main.cpp
@@ -3,6 +3,7 @@
 #include <regex>
 #include <chrono>
 #include <numeric>
+#include <valarray>
 
 using namespace std;
 
@@ -10,60 +11,38 @@ template <typename T> int sgn(T val) {
     return (T(0) < val) - (val < T(0));
 }
 
-void displ(auto itrbl) {
-    for (auto val : itrbl) {
-        cout<<(val ? '#' : ' ')<<", ";
+void displ(auto & va, int x, int y)
+{
+    for (auto yi=0; yi<y; ++yi) {
+        for (auto xi=0; xi<x; ++xi)
+            cout<<(va[yi*x + xi] > 0 ? '#' : ' ')<<", ";
+        cout<<'\n';
     }
-    cout<<'\n';
-}
 
-void displl(auto itrbl) {
-    for (auto val : itrbl)
-        displ(val);
-    cout<<'\n';
 }
-
-void rotate_row(array<array<bool, 50>, 6> & screen,
-        size_t row, size_t count)
-{
-    rotate(screen[row].begin(), screen[row].end()-count, screen[row].end()); 
-}
-
-void rotate_col(array<array<bool, 50>, 6> & screen,
-        size_t col, size_t count)
-{
-    array<bool, 6> orig_col;
-    for (size_t i=0; i<6; ++i)
-        orig_col[i] = screen[i][col];
-    rotate(orig_col.begin(), orig_col.end()-count, orig_col.end()); 
-    for (size_t i=0; i<6; ++i)
-        screen[i][col] = orig_col[i];
-}
-
 
 string run1(ifstream ifs)
 {
-    unordered_map<string, function<decltype(rotate_row)>> rot_funs 
-    {{"column", rotate_col}, {"row", rotate_row}};
-    array<array<bool, 50>, 6> screen {false};
-    regex rect_pat {R"(rect (\d+)x(\d+))"};
-    regex rot_pat {R"(rotate (row|column) (x|y)=(\d+) by (\d+))"};
-    smatch sm;
+    size_t X = 50, Y = 6;
+    valarray<int> screen(X*Y);
+    auto auto_slice = [X,Y](char roc, int i) {
+        return roc == 'y' ? slice(i*X, X, 1) : slice(i, Y, X);};
+
     for (string line; getline(ifs, line);) {
-        if (regex_match(line, sm, rect_pat)) {
-            for (size_t row=0; row<stoul(sm[2].str()); ++row)
-                for (size_t col=0; col<stoul(sm[1].str()); ++col)
-                    screen[row][col] = true;
-            displl(screen);
-        } else if (regex_match(line, sm, rot_pat)) {
-            rot_funs[sm[1].str()](screen, stoul(sm[3]), stoul(sm[4]));
-            displl(screen);
+        unsigned int x,y;
+        char c;
+            sscanf(line.c_str(), "rotate %*s %c=%d by %d", &c, &x, &y) ||
+        sscanf(line.c_str(), "%c%*s %dx%d", &c, &x, &y);
+        if (c=='r')
+            screen[gslice(0, {y,x}, {X,1})] = 1;
+        else {
+            screen[auto_slice(c, x)] = 
+                valarray<int> {screen[auto_slice(c, x)]}.cshift(-y);
         }
+           
     }
-    int res = accumulate(screen.begin(), screen.end(), 0, 
-            [] (auto acc, auto & lhs) {
-            return acc + accumulate(lhs.begin(), lhs.end(), 0);});
-    return to_string(res);
+    displ(screen, X, Y);
+    return to_string(screen.sum());
 }
 
 string run2(ifstream ifs)
