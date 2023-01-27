d58017950778ae42847cdd76f0a27e2d2ed212bd
dmad <dmad@t14g3.fritz.box>
2023-01-27 18:03:06 +0100

    switched to bfs, results for part1 but two too slow

--- 11/p.py
+++ 11/p.py
@@ -1,7 +1,6 @@
 from timeit import default_timer as timer
 import sys
 import re
-from copy import deepcopy
 from itertools import combinations
 from collections import defaultdict
 
@@ -54,48 +53,33 @@ def safe(state):
 def combis(state):
     items = [1<<i for i in range(no_mics*2) 
             if state[2] & 1<<(bitf(state[1])+i)]
-    return items + [a+b for a,b in combinations(items,2)]
+    return [a+b for a,b in combinations(items,2)] + items 
 
 
-best_steps = 9999999
-visited = defaultdict(lambda:9999999)
-
-def dfs(state):
-    if visited[tuple(state[1:])] < state[0]:
-        return
-    visited[tuple(state[1:])] = state[0]
-    pp(state)
-    global best_steps
-    print(f'best_steps : {best_steps }')
-    if best_steps < state[0]:
-        print("skipping")
-        return
-
-    print(bin(two_mics_ones << bitf(3)))
-    print(bin(state[2]))
-    print(bin(two_mics_ones << bitf(3) & state[2]))
-    if (two_mics_ones << bitf(3)) & state[2] == two_mics_ones << bitf(3):
-        print("found one solution")
-        best_steps = min(state[0], best_steps)
-
-
-    for tf in [f for f in [state[1] + 1, state[1] -1] if 0<=f<=3]:
-        print(f'tf : {tf }')
-        print(f'combis(state): {combis(state)}')
-        for cb in combis(state):
-            print(f'cb : {cb }')
-            sc = state.copy()
-            sc[0] += 1
-            sc[1] = tf
-            sc[2] |= cb << bitf(tf)
-            sc[2] &= ~(cb << bitf(state[1]))
-            print('iiiiiiiiiiiiiiiiiiii')
-            pp(state)
-            pp(sc)
-            print('iiiiiiiiiiiiiiiiiiii')
-            if safe(sc):
-                print("doing smth")
-                dfs(sc)
+#best_steps = 9999999
+#visited = defaultdict(lambda:9999999)
+#
+#def dfs(state):
+#    global best_steps, visited
+#    if visited[tuple(state[1:])] < state[0]:
+#        return
+#    visited[tuple(state[1:])] = state[0]
+#    if best_steps <= state[0]:
+#        return
+#
+#    if (two_mics_ones << bitf(3)) & state[2] == two_mics_ones << bitf(3):
+#        print("found one: ", best_steps)
+#        best_steps = min(state[0], best_steps)
+#
+#    for tf in [f for f in [state[1] + 1, state[1] -1] if 0<=f<=3]:
+#        for cb in combis(state):
+#            sc = state.copy()
+#            sc[0] += 1
+#            sc[1] = tf
+#            sc[2] |= cb << bitf(tf)
+#            sc[2] &= ~(cb << bitf(state[1]))
+#            if safe(sc):
+#                dfs(sc)
 
 
 def main1(f):
@@ -117,18 +101,78 @@ def main1(f):
         state[2] |= 1<<bitm(i, value)
         state[2] |= 1<<bitg(i, gens[key])
 
+    todo = [state]
+    visited = defaultdict(lambda:9999999)
+    while len(todo) > 0:
+        state = todo.pop(0)
+        if visited[tuple(state[1:])] <= state[0]:
+            continue
+        visited[tuple(state[1:])] = state[0]
 
-    #for i in range(no_mics):
-    #    state[2] |= 1<<bitg(i, 3)
-    #    state[2] |= 1<<bitm(i, 3)
-    dfs(state)
+        if (two_mics_ones << bitf(3)) & state[2] == two_mics_ones << bitf(3):
+            return str(state[0])
+
+        for tf in [f for f in [state[1] + 1, state[1] -1] if 0<=f<=3]:
+            for cb in combis(state):
+                sc = state.copy()
+                sc[0] += 1
+                sc[1] = tf
+                sc[2] |= cb << bitf(tf)
+                sc[2] &= ~(cb << bitf(state[1]))
+                if safe(sc):
+                    todo.append(sc)
+
     return str(best_steps)
 
 
 def main2(f):
-    line = f.read()
+    gens = {}
+    mics = {}
+    for floor, line in enumerate(f):
+        for mic in mic_pat.findall(line):
+            mics[mic] = floor
+        for gen in gen_pat.findall(line):
+            gens[gen] = floor
 
-    return ""
+    mics['a'] = 3
+    gens['a'] = 3
+    mics['b'] = 2
+    gens['b'] = 2
+    global no_mics, no_mics_ones, two_mics_ones
+
+    no_mics = len(mics)
+    no_mics_ones = (2**no_mics -1)
+    two_mics_ones = (2**(2*no_mics) -1)
+    #[steps, elev, poss]
+    state = [0, 0, 0]
+    for i, (key, value) in enumerate(mics.items()):
+        state[2] |= 1<<bitm(i, value)
+        state[2] |= 1<<bitg(i, gens[key])
+
+    if not safe(state):
+        print("fail")
+    todo = [state]
+    visited = defaultdict(lambda:9999999)
+    while len(todo) > 0:
+        state = todo.pop(0)
+        if visited[tuple(state[1:])] <= state[0]:
+            continue
+        visited[tuple(state[1:])] = state[0]
+
+        if (two_mics_ones << bitf(3)) & state[2] == two_mics_ones << bitf(3):
+            return str(state[0])
+
+        for tf in [f for f in [state[1] + 1, state[1] -1] if 0<=f<=3]:
+            for cb in combis(state):
+                sc = state.copy()
+                sc[0] += 1
+                sc[1] = tf
+                sc[2] |= cb << bitf(tf)
+                sc[2] &= ~(cb << bitf(state[1]))
+                if safe(sc):
+                    todo.append(sc)
+
+    return str("impossible")
 
 
 def main():
