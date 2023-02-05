a1ec5239c91d667224670b59b3b5f1c2c2884ff0
dmad <dmad@t14g3.fritz.box>
2023-02-05 16:48:24 +0100

    fancy bitwise solution from aralski reddit

--- 18/main.cpp
+++ 18/main.cpp
@@ -23,13 +23,9 @@ string run1(ifstream ifs)
         string next {"."};
         for (size_t i=1; i<cur.size()-1; ++i) {
             auto l = cur[i-1] == '^';
-            auto c = cur[i] == '^';
             auto r = cur[i+1] == '^';
-            if ((l && c && !r) ||
-                    (!l && c && r) ||
-                    (l && !c && !r) ||
-                    (!l && !c && r))
-                next.push_back('^');
+            if (l != r)
+                    next.push_back('^');
             else
                 next.push_back('.');
         }
@@ -45,9 +41,32 @@ string run1(ifstream ifs)
 
     return to_string(ret);
 }
+//Bitwise using 128-bit integers in C. Compile with -march=native
+//
+int solve(__uint128_t traps, __uint128_t mask, int rows)
+{
+    int n_safe = 0;
+    while (rows--) {
+        n_safe += __builtin_popcountl((uint64_t) ((mask ^ traps) >> 64));
+        n_safe += __builtin_popcountl((uint64_t)  (mask ^ traps));
+        traps = (traps << 1) ^ (traps >> 1);
+        traps &= mask;
+    }
+    return n_safe;
+}
 
 string run2(ifstream ifs)
 {
+    string line;
+    getline(ifs, line);
+    __uint128_t traps = 0, mask = 0;
+
+    for (auto c : line) {
+        traps = (traps << 1) | (c == '^');
+        mask  = (mask  << 1) | 1;
+    }
+    printf("Part 2: %d\n", solve(traps, mask, 400000));
+
     return to_string(0);
 }
 
